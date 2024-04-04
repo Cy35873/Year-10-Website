374a7680e37a22eb509bec87cb111eae23a91aa2
Cy35873 <160684566+Cy35873@users.noreply.github.com>
2024-04-05 10:17:25 +1300

    fixed the video and images

--- video.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function() {
-  var videoContainer = document.getElementById('video-container');
-  if (videoContainer) {
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    videoContainer.style.top = 50 + scrollPosition + "%";
-  }
-};
